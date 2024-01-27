7c33547f28b3e98e3d3335c8ab9363c2aad2fb3d
Rutger <rutger.pronk11@gmail.com>
2024-01-27 23:48:35 +0100

    Change the animation of the copy email

--- src/components/contact/ContactDescription.tsx
+++ src/components/contact/ContactDescription.tsx
@@ -9,9 +9,9 @@ export default function ContactDescription() {
   const [mailText, setMailText] = React.useState(t("clickToCopy"));
 
   function handleCopyEmail(e: React.MouseEvent<HTMLSpanElement>) {
-    const email = e.currentTarget.innerText.slice(
+    const email = e.currentTarget.innerHTML.slice(
       0,
-      e.currentTarget.innerText.indexOf(" "),
+      e.currentTarget.innerHTML.indexOf("<"),
     );
     navigator.clipboard.writeText(email);
     setMailText(t("copied"));
@@ -26,15 +26,21 @@ export default function ContactDescription() {
               "translate group relative inline-flex flex-col items-center"
             }
             onClick={handleCopyEmail}
-            onMouseLeave={() => setMailText(t("clickToCopy"))}
+            onMouseEnter={() => setMailText(t("clickToCopy"))}
           >
             {children}
             <span
               className={
-                "pointer-events-none absolute -translate-y-11 rounded-md px-2 py-1 opacity-0 backdrop-blur-md transition-all duration-150 group-hover:-translate-y-8 group-hover:opacity-100"
+                "pointer-events-none absolute block h-fit w-fit -translate-y-8 overflow-hidden"
               }
             >
-              {mailText}
+              <span
+                className={
+                  " block translate-y-7 rounded-md px-2 py-1 backdrop-blur duration-150 group-hover:translate-y-0 "
+                }
+              >
+                {mailText}
+              </span>
             </span>
           </span>
         ),
