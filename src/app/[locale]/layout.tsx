a7578fc9958b8739e5d2afb9a8d0ac05e18138ac
Rutger <rutger.pronk11@gmail.com>
2024-02-03 14:36:48 +0100

    refactor: remove whitespace before classes

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang={locale}>
-      <body className={` ${inter.className} bg-primary`}>{children}</body>
+      <body className={`${inter.className} bg-primary`}>{children}</body>
     </html>
   );
 }
