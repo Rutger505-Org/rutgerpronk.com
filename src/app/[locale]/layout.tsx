c4c1cff2ee59d310e697607c3496ed0b22e34871
Rutger <rutger.pronk11@gmail.com>
2024-02-03 14:48:52 +0100

    Feat: add metadata

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,12 +2,39 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import React from "react";
+import { getLocale, getTranslations } from "next-intl/server";
+import { locales } from "../../../i18n.config";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Rutger Pronk - Portfolio",
-};
+export async function generateMetadata(): Promise<Metadata> {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const t = await getTranslations("metadata");
+  const locale = await getLocale();
+  const alternateLocales = locales.filter((l) => l !== locale);
+
+  return {
+    title: t("title"),
+    description: t("description"),
+    keywords: [
+      "Rutger Pronk",
+      "Rutger",
+      "Pronk",
+      "Software Developer",
+      "Portfolio",
+      "Next.js",
+      "TypeScript",
+    ],
+    openGraph: {
+      title: t("title"),
+      description: t("description"),
+      type: "website",
+      locale: locale,
+      alternateLocale: alternateLocales,
+      url: "https://rutgerpronk.com",
+    },
+  };
+}
 
 export default function RootLayout({
   children,
