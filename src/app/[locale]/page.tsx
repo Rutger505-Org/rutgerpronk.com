ca32471ffb7098b9678366c48be26ebfe2a3a32e
Rutger Pronk <rutger.pronk11@gmail.com>
2025-09-07 01:56:27 +0200

    Undo parallax change

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -5,40 +5,18 @@ import Projects from "@/components/projects";
 import Contact from "@/components/contact";
 import MobileHeader from "@/components/header/MobileHeader";
 import DesktopHeader from "@/components/header/DesktopHeader";
-import ThreeBackground from "@/components/ThreeBackground";
 
 export default function Home() {
   return (
     <>
       <DesktopHeader />
       <MobileHeader />
-
-      <ThreeBackground />
-
-      <main>
-        <div className="mx-spacing-mobile max-w-[2300px] sm:mx-spacing too-big:mx-auto">
-          <LandingSection />
-        </div>
-
-        <div className={"transition-stack"}></div>
-
-        <div className=" bg-[#1c1b26] ">
-          <div className="mx-spacing-mobile max-w-[2300px] sm:mx-spacing too-big:mx-auto">
-            <About />
-          </div>
-        </div>
-
-        <div className={"transition-stack transition-stack-reverse"}></div>
-
-        <div className="mx-spacing-mobile max-w-[2300px] sm:mx-spacing too-big:mx-auto">
-          <Projects />
-        </div>
-
-        <div className="mx-spacing-mobile max-w-[2300px] sm:mx-spacing too-big:mx-auto">
-          <Contact />
-        </div>
+      <main className="mx-spacing-mobile max-w-[2300px] sm:mx-spacing too-big:mx-auto">
+        <LandingSection />
+        <About />
+        <Projects />
+        <Contact />
       </main>
-
       <Footer />
     </>
   );
