440832137450f80b818c12943cf2d5b6f51a98e7
Rutger Pronk <rutger.pronk11@gmail.com>
2025-09-07 02:49:28 +0200

    Add the parallax effect

--- src/components/LandingSection.tsx
+++ src/components/LandingSection.tsx
@@ -1,25 +1,42 @@
-import React from "react";
+"use client";
+
+import React, { useRef } from "react";
 import ScrollLink from "@/components/ScrollLink";
 import ArrowDownIcon from "@/components/icons/ArrowDownIcon";
 import { useTranslations } from "next-intl";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 export default function LandingSection() {
   const t = useTranslations("landingSection");
 
+  const ref = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+  const textY = useTransform(scrollYProgress, [0, 1], ["0%", "500%"]);
+
+  console.log(scrollYProgress, textY);
+
   return (
     <section
+      ref={ref}
       id={"home"}
       className={"flex min-h-screen flex-col items-center justify-between"}
     >
       <div></div>
-
       <div className={"max-w-full"}>
-        <h2 className={"text-center text-3xl text-textPrimary sm:text-4xl"}>
+        <motion.h2
+          style={{ y: textY }}
+          className={"text-center text-3xl text-textPrimary sm:text-4xl"}
+        >
           {t.rich("softwareDev", {
             symbol: "</>",
           })}
-        </h2>
-        <h1
+        </motion.h2>
+        <motion.h1
+          style={{ y: textY }}
           className={
             "mb-10 max-w-2xl text-center text-5xl font-bold text-textPrimary sm:text-6xl"
           }
@@ -29,7 +46,7 @@ export default function LandingSection() {
               <span className={"text-accent"}>{children}</span>
             ),
           })}
-        </h1>
+        </motion.h1>
       </div>
 
       <ScrollLink href={"#about"} to={"about"} className={"mb-6"}>
